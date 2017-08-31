e680d64f77ee176c2a6f73bdd5e0de1ee2eca025
Ronald van den Bos <ronald_van_den_bos@nl.ibm.com>
2017-08-31 10:57:44 +0200

    new branch for splitting up files

--- app.js
+++ app.js
@@ -14,15 +14,30 @@ const argv = yargs
   .alias('help', 'h')
   .argv;
 
-console.log(argv.a);
+var encodedAddress = encodeURIComponent(argv.a);
 
 request({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=ravelijn%202%20vijfhuizen',
+  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
   json: true
 },(error, response, body) => {
-  // console.log(`Address: ${body.results[0].formatted_address}`);
-  // console.log(`GeoCode latitude: ${body.results[0].geometry.location.lat}`);
-  // console.log(`GeoCode longtitude: ${body.results[0].geometry.location.lng}`);
+
+  if (error) {
+    console.log();
+    console.log('Unable to connect to Google Servers');
+    console.log();
+  } else if (body.status === 'ZERO_RESULTS') {
+    console.log();
+    console.log('Sorry, we were unable to find that address')
+    console.log();
+  } else if (body.status === 'OK') {
+    console.log();
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`GeoCode latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`GeoCode longtitude: ${body.results[0].geometry.location.lng}`);
+    console.log();
+  }
+
+
 
 });
 
